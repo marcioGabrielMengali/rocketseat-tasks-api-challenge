9f528c5e6bbcd850b32d9e83e330f410e220a0f2
Marcio gabriel Mengali <marciogabrielmengali@MacBook-Air-de-Marcio.local>
2024-11-27 15:19:43 -0300

    feat: create body validation

--- src/routes/handlers.js
+++ src/routes/handlers.js
@@ -1,8 +1,16 @@
+import { bodyValidation } from "../utils/body-validation.js";
+
 export const getTaksks = (req, res) => {
-    return res.writeHead(200).end('getTasks')
-}
+  return res.writeHead(200).end("getTasks");
+};
 
 export const createTask = (req, res) => {
-    console.log({body: req.body});
-    return res.writeHead(201).end('createTasks')
-}
+  const { body } = req;
+  const validation = bodyValidation(body);
+  if (validation) {
+    console.log({ body });
+    return res.writeHead(201).end("createTasks");
+  } else {
+    return res.writeHead(400).end("Bad Request");
+  }
+};
